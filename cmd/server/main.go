e32aeb3bc447fe1a4992720ad05d018c900fd6d2
shadyaziza <shadyaziza@gmail.com>
2022-07-09 20:03:45 +0200

    add app struct for configs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers
+// to database connections
+type App struct {
+}
+
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting up api")
+	return nil
+}
 func main() {
 	fmt.Println("rest")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up api")
+	}
 }
