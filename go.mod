fbd5d57180f3ad840b61ab7b6dff47c987273c67
shadyaziza <shadyaziza@gmail.com>
2022-07-14 12:56:53 +0200

    fix dependancies and add migration function

--- go.mod
+++ go.mod
@@ -4,4 +4,13 @@ go 1.18
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/lib/pq v1.10.6
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/lib/pq v1.10.6
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
---
+++ internal/db/migrate.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func (d *Database) MigrateDB() error {
+
+	fmt.Println("Migrating the database ... ")
+	driver, err := postgres.WithInstance(d.Client, &postgres.Config{})
+	if err != nil {
+		fmt.Errorf("Could not create the postgres driver: %w", err)
+
+	}
+	m, err := migrate.NewWithDatabaseInstance("file:///migrations", "postgres", driver)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if err := m.Up(); err != nil {
+		return fmt.Errorf("Could not run up the migrations: %w", err)
+	}
+	fmt.Println("Successfully migrated the database")
+	return nil
+}
